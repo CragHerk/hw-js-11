37bd5edc909e1341d8a646df15fdc0a258b878c1
m.backi <backimichal@gmail.com>
2023-04-10 21:12:42 +0200

    działa

--- src/getImages.js
+++ src/getImages.js
@@ -6,7 +6,7 @@ const API_KEY = '35097594-2079bd5e4adb4155ba76a6246';
 const getImages = async searchTerm => {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo`
+      `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo&per_page=40`
     );
     const hits = response.data.hits;
     if (hits.length === 0) {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,71 @@
 import getImages from './getImages';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+const perPage = 40;
 
 const createGallery = images => {
   const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = '';
 
   images.forEach(image => {
-    const photoCard = document.createElement('div');
-    photoCard.classList.add('photo-card');
-    photoCard.innerHTML = `
-      <a href="${image.largeImageURL}" data-caption="${image.tags}">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-      </a>
-    `;
-    gallery.appendChild(photoCard);
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery-item');
+
+    const a = document.createElement('a');
+    a.href = image.webformatURL;
+    a.classList.add('gallery-link');
+
+    const img = document.createElement('img');
+    img.src = image.webformatURL;
+    img.alt = image.tags;
+    img.classList.add('gallery-img');
+    a.appendChild(img);
+
+    const imgInfo = document.createElement('div');
+    imgInfo.classList.add('img-info');
+
+    const likesLabel = document.createElement('div');
+    likesLabel.innerText = 'Likes: ';
+
+    const likesCount = document.createElement('span');
+    likesCount.innerText = image.likes.toLocaleString();
+
+    const viewsLabel = document.createElement('div');
+    viewsLabel.innerText = 'Views: ';
+
+    const viewsCount = document.createElement('span');
+    viewsCount.innerText = image.views.toLocaleString();
+
+    const commentsLabel = document.createElement('div');
+    commentsLabel.innerText = 'Comments: ';
+
+    const commentsCount = document.createElement('span');
+    commentsCount.innerText = image.comments.toLocaleString();
+
+    const downloadsLabel = document.createElement('div');
+    downloadsLabel.innerText = 'Downloads: ';
+
+    const downloadsCount = document.createElement('span');
+    downloadsCount.innerText = image.downloads.toLocaleString();
+
+    likesLabel.appendChild(likesCount);
+    viewsLabel.appendChild(viewsCount);
+    commentsLabel.appendChild(commentsCount);
+    downloadsLabel.appendChild(downloadsCount);
+
+    imgInfo.appendChild(likesLabel);
+    imgInfo.appendChild(viewsLabel);
+    imgInfo.appendChild(commentsLabel);
+    imgInfo.appendChild(downloadsLabel);
+
+    a.appendChild(imgInfo);
+
+    galleryItem.appendChild(a);
+    gallery.appendChild(galleryItem);
   });
 
-  const lightbox = new SimpleLightbox('.gallery a');
+  const lightbox = new SimpleLightbox('.gallery a', {
+    /* opcje */
+  });
   lightbox.on('error.simplelightbox', function (e) {
     console.log(e);
   });
