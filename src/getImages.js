8aed55f076d91bdcafee8adcdfdd585e75bc65ca
m.backi <backimichal@gmail.com>
2023-04-11 17:50:22 +0200

    Update getImages.js

--- src/getImages.js
+++ src/getImages.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 import notiflix from 'notiflix';
 
 const API_KEY = '35097594-2079bd5e4adb4155ba76a6246';
+const PER_PAGE = 40;
 
-const getImages = async searchTerm => {
+const getImages = async (searchTerm, page) => {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo&per_page=40`
+      `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo&per_page=${PER_PAGE}&page=${page}`
     );
     const hits = response.data.hits;
     if (hits.length === 0) {
